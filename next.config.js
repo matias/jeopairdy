8ba0184aede0eca6ee235d0d75250b909065c6d2
Matias Pelenur <matias@pelenur.com>
2025-11-16 19:47:59 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['192.168.1.24'],
+  // allowedDevOrigins can be configured via environment variable if needed
+  // allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS?.split(',') || [],
 }
 
 module.exports = nextConfig
